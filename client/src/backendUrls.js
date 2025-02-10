ef38943519c977802e2bfb4b0937f6b0c3f8a0cf
whitezkkaka <minhaconta54321rs@gmail.com>
2025-02-09 22:15:57 -0300

    Update backendUrls.js

--- client/src/backendUrls.js
+++ client/src/backendUrls.js
@@ -1,6 +1,6 @@
 const backendUrls = {
   http: 'https://chatqualquer.onrender.com',
-  ws: 'wss://https://chatqualquer.onrender.com/graphql',
+  ws: 'wss://chatqualquer.onrender.com/graphql',
 };
 
 export default backendUrls;
